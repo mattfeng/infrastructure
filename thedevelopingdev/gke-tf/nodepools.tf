052d331d2ee8692b69f5f230f6301466566f8c70
mattfeng <mattfeng@mit.edu>
2020-09-24 12:41:04 -0400

    update infra to add more scopes

--- thedevelopingdev/gke-tf/nodepools.tf
+++ thedevelopingdev/gke-tf/nodepools.tf
@@ -20,6 +20,7 @@ resource "google_container_node_pool" "worker" {
     oauth_scopes = [
       "https://www.googleapis.com/auth/logging.write",
       "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/compute",
       "https://www.googleapis.com/auth/monitoring"
     ]
 
